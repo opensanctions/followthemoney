8000be50271a065977e308314d7639e67d568de3
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-12-30 14:20:59 +0100

    Make `statement.clone()` handle logic of re-hashing

--- followthemoney/statement/statement.py
+++ followthemoney/statement/statement.py
@@ -1,8 +1,8 @@
 import hashlib
 import warnings
 from sqlalchemy.engine import Row
-from typing import cast
-from typing import Any, Dict, Generator, Optional
+from typing import Union, cast
+from typing import Any, Dict, Generator, Optional, TypeGuard
 from typing_extensions import TypedDict, Self
 from rigour.time import datetime_iso, iso_datetime
 from rigour.boolean import bool_text
@@ -10,6 +10,13 @@ from rigour.boolean import bool_text
 from followthemoney.proxy import EntityProxy
 from followthemoney.statement.util import get_prop_type, BASE_ID
 from followthemoney.util import HASH_ENCODING
+
+
+UNSET = object()
+
+
+def is_not_unset(value: str | None | object) -> TypeGuard[str | None]:
+    return value is not UNSET
 
 
 class StatementDict(TypedDict):
@@ -43,15 +50,15 @@ class Statement(object):
 
     __slots__ = [
         "id",
-        "entity_id",
+        "_entity_id",
         "canonical_id",
-        "prop",
-        "schema",
-        "value",
-        "dataset",
-        "lang",
+        "_prop",
+        "_schema",
+        "_value",
+        "_dataset",
+        "_lang",
         "original_value",
-        "external",
+        "_external",
         "first_seen",
         "last_seen",
         "origin",
@@ -73,55 +80,90 @@ class Statement(object):
         last_seen: Optional[str] = None,
         origin: Optional[str] = None,
     ):
-        self.entity_id = entity_id
+        self._entity_id = entity_id
         self.canonical_id = canonical_id or entity_id
-        self.prop = prop
-        self.schema = schema
-        self.value = value
-        self.dataset = dataset
-        self.lang = lang
+        self._prop = prop
+        self._schema = schema
+        self._value = value
+        self._dataset = dataset
+        self._lang = lang
         self.original_value = original_value
         self.first_seen = first_seen
         self.last_seen = last_seen or first_seen
-        self.external = external
+        self._external = external
         self.origin = origin
         if id is None:
             id = self.generate_key()
         self.id = id
 
     @property
+    def entity_id(self) -> str:
+        """The (original) ID of the entity this statement is about."""
+        return self._entity_id
+
+    @property
+    def dataset(self) -> str:
+        """The dataset this statement was observed in."""
+        return self._dataset
+
+    @property
+    def prop(self) -> str:
+        """The property name this statement is about."""
+        return self._prop
+
+    @property
+    def schema(self) -> str:
+        """The schema of the entity this statement is about."""
+        return self._schema
+
+    @property
+    def value(self) -> str:
+        """The value of the property captured by this statement."""
+        return self._value
+
+    @property
+    def lang(self) -> Optional[str]:
+        """The language of the property value, if applicable."""
+        return self._lang
+
+    @property
+    def external(self) -> bool:
+        """Whether this statement was observed in an external dataset."""
+        return self._external
+
+    @property
     def prop_type(self) -> str:
         """The type of the property, e.g. 'string', 'number', 'url'."""
-        return get_prop_type(self.schema, self.prop)
+        return get_prop_type(self._schema, self._prop)
 
     def to_dict(self) -> StatementDict:
         return {
             "canonical_id": self.canonical_id,
-            "entity_id": self.entity_id,
-            "prop": self.prop,
-            "schema": self.schema,
-            "value": self.value,
-            "dataset": self.dataset,
-            "lang": self.lang,
+            "entity_id": self._entity_id,
+            "prop": self._prop,
+            "schema": self._schema,
+            "value": self._value,
+            "dataset": self._dataset,
+            "lang": self._lang,
             "original_value": self.original_value,
             "first_seen": self.first_seen,
             "last_seen": self.last_seen,
-            "external": self.external,
+            "external": self._external,
             "origin": self.origin,
             "id": self.id,
         }
 
     def to_csv_row(self) -> Dict[str, Optional[str]]:
         data = cast(Dict[str, Optional[str]], self.to_dict())
-        data["external"] = bool_text(self.external)
-        data["prop_type"] = get_prop_type(self.schema, self.prop)
+        data["external"] = bool_text(self._external)
+        data["prop_type"] = get_prop_type(self._schema, self._prop)
         return data
 
     def to_db_row(self) -> Dict[str, Any]:
         data = cast(Dict[str, Any], self.to_dict())
         data["first_seen"] = iso_datetime(self.first_seen)
         data["last_seen"] = iso_datetime(self.last_seen)
-        data["prop_type"] = get_prop_type(self.schema, self.prop)
+        data["prop_type"] = get_prop_type(self._schema, self._prop)
         return data
 
     def __hash__(self) -> int:
@@ -133,28 +175,81 @@ class Statement(object):
         return hash(self.id)
 
     def __repr__(self) -> str:
-        return "<Statement(%r, %r, %r)>" % (self.entity_id, self.prop, self.value)
+        return "<Statement(%r, %r, %r)>" % (self._entity_id, self._prop, self._value)
 
     def __eq__(self, other: Any) -> bool:
         return not self.id != other.id
 
     def __lt__(self, other: Any) -> bool:
-        self_key = (self.prop != BASE_ID, self.id or "")
-        other_key = (other.prop != BASE_ID, other.id or "")
+        self_key = (self._prop != BASE_ID, self.id or "")
+        other_key = (other._prop != BASE_ID, other.id or "")
         return self_key < other_key
 
-    def clone(self: Self) -> "Statement":
+    def clone(
+        self: Self,
+        *,
+        entity_id: Optional[str] = None,
+        prop: Optional[str] = None,
+        schema: Optional[str] = None,
+        value: Optional[str] = None,
+        dataset: Optional[str] = None,
+        lang: Union[str, None, object] = UNSET,
+        original_value: Union[str, None, object] = UNSET,
+        first_seen: Union[str, None, object] = UNSET,
+        external: Optional[bool] = None,
+        canonical_id: Optional[str] = None,
+        last_seen: Union[str, None, object] = UNSET,
+        origin: Union[str, None, object] = UNSET,
+    ) -> "Statement":
         """Make a deep copy of the given statement."""
-        return Statement.from_dict(self.to_dict())
+        lang = lang if is_not_unset(lang) else self._lang
+        ov = original_value if is_not_unset(original_value) else self.original_value
+        first_seen = first_seen if is_not_unset(first_seen) else self.first_seen
+        last_seen = last_seen if is_not_unset(last_seen) else self.last_seen
+        origin = origin if is_not_unset(origin) else self.origin
+        if external is None:
+            external = self._external
+        if canonical_id is None and self._entity_id != self.canonical_id:
+            canonical_id = self.canonical_id
+
+        # Decide if the statement ID can be kept the same:
+        stmt_id = self.id
+        if entity_id is not None and entity_id != self.entity_id:
+            stmt_id = None
+        if prop is not None and prop != self._prop:
+            stmt_id = None
+        if schema is not None and schema != self._schema:
+            stmt_id = None
+        if value is not None and value != self._value:
+            stmt_id = None
+        if dataset is not None and dataset != self._dataset:
+            stmt_id = None
+        if lang != self._lang:
+            stmt_id = None
+        return Statement(
+            id=stmt_id,
+            entity_id=entity_id or self._entity_id,
+            prop=prop or self._prop,
+            schema=schema or self._schema,
+            value=value or self._value,
+            dataset=dataset or self._dataset,
+            lang=lang,
+            original_value=ov,
+            first_seen=first_seen,
+            external=external,
+            canonical_id=canonical_id,
+            last_seen=last_seen,
+            origin=origin,
+        )
 
     def generate_key(self) -> Optional[str]:
         return self.make_key(
-            self.dataset,
-            self.entity_id,
-            self.prop,
-            self.value,
-            self.external,
-            lang=self.lang,
+            self._dataset,
+            self._entity_id,
+            self._prop,
+            self._value,
+            self._external,
+            lang=self._lang,
         )
 
     @classmethod
@@ -173,7 +268,7 @@ class Statement(object):
         if lang is None:
             key = f"{dataset}.{entity_id}.{prop}.{value}"
         else:
-            key = f"{dataset}.{entity_id}.{prop}.{value}.{lang}"
+            key = f"{dataset}.{entity_id}.{prop}.{value}@{lang}"
         if external:
             # We consider the external flag in key composition to avoid race conditions
             # where a certain entity might be emitted as external while it is already
