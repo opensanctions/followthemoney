4694eeb690d7f37ed94dd1be2570ef992ab10060
Friedrich Lindenberg <friedrich@opensanctions.org>
2026-07-14 14:57:03 +0200

    Bump version: 4.9.2 → 4.10.0

--- followthemoney/__init__.py
+++ followthemoney/__init__.py
@@ -9,7 +9,7 @@ from followthemoney.statement import Statement, StatementEntity, SE
 from followthemoney.dataset import Dataset, UndefinedDataset, DS
 from followthemoney.util import set_model_locale
 
-__version__ = "4.9.2"
+__version__ = "4.10.0"
 
 # Data model singleton
 model = Model.instance()
